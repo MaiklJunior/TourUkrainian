59b69817812a2cff5fc4956978742bcfa11e1be3
MaiklJunior <117854368+MaiklJunior@users.noreply.github.com>
2022-11-09 20:06:10 +0200

    New Site
    Site

---
+++ Action_Work.js
@@ -0,0 +1,28 @@
+var Georgia = 'Георгій'
+Elizabet = 'Елізаветта'
+console.log('Сьогодні одружились'+' '+Georgia+' '+'та'+' '+Elizabet);
+var UserPersonality = {
+  name:"Mihail",
+  lastname: "Kravtsov",
+  age: 19
+
+}
+
+UserPersonality.age=20;
+UserPersonality.name="Mihail";
+UserPersonality.lastname="Kravtsov";
+console.log(UserPersonality.name, UserPersonality.lastname, UserPersonality.age=20);
+let Flower = document.write();
+function florial(){
+  var Colors = 'red'
+  Colors2 = 'blue'
+  Colors3 = 'fiolet';
+  Flower;
+  if(Flower = Colors){
+    console.log("Ваші квіти червоні");
+  }
+  if(Flower = Colors2){
+    console.log("Ваші квіти сині");
+  }
+}
+florial();
